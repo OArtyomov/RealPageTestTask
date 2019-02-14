11c453cf56706a83d36d4c9bc5b522cac5fd5da5
OlegArtyomov <o.artyomov@yaypay.com>
2019-02-14 14:32:00 +0200

    Append handling of exit from application

--- src/main/java/com/realpage/calculator/Application.java
+++ src/main/java/com/realpage/calculator/Application.java
@@ -6,9 +6,11 @@ import com.realpage.calculator.service.CommandRegistry;
 import com.realpage.calculator.service.ExecutionContext;
 import com.realpage.calculator.utils.Consoler;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import static com.realpage.calculator.utils.StringUtils.isDouble;
+import static com.realpage.calculator.utils.StringUtils.isNotEmpty;
 import static java.lang.Double.parseDouble;
 
 public class Application {
@@ -21,20 +23,28 @@ public class Application {
 
 
     public static void main(String[] args) {
-        while (true) {
+        boolean needExit = false;
+        while (!needExit) {
             consoler.print(">");
             Scanner scanner = new Scanner(System.in);
-            String inputValue = scanner.next();
-            AbstractCommand command = commandFinder.findCommand(inputValue);
-            if (command != null) {
-                command.execute(executionContext);
-            } else {
-                boolean isData = isDouble(inputValue);
-                if (isData) {
-                    executionContext.addNumber(parseDouble(inputValue));
-                    consoler.println(inputValue);
+            String inputValue = null;
+            try {
+                inputValue = scanner.next();
+            } catch (NoSuchElementException e) {
+                needExit = true;
+            }
+            if (isNotEmpty(inputValue)) {
+                AbstractCommand command = commandFinder.findCommand(inputValue);
+                if (command != null) {
+                    command.execute(executionContext);
                 } else {
-                    consoler.println("Command for execution not found or data is invalid");
+                    boolean isData = isDouble(inputValue);
+                    if (isData) {
+                        executionContext.addNumber(parseDouble(inputValue));
+                        consoler.println(inputValue);
+                    } else {
+                        consoler.println("Command for execution not found or data is invalid");
+                    }
                 }
             }
         }
